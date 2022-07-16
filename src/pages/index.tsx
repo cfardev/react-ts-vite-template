d47d140cc07f2a85371dc1da988f1b3a9aa72db5
Carlos Arcia <cfar.dev@gmail.com>
2022-07-15 18:48:49 -0600

    Eslint, Prettier, Husky Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
 
 export default App;
